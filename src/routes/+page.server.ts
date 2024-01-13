ffc01f42f2dcd4f60bfab787abde8655bbd6eb4e
Neville Brem <contact@nevillebrem.com>
2024-01-13 19:38:03 +0100

    perf: removed unused imports

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,8 @@
 import { db } from '$lib/server/db/db';
-import { postsTable, usersTable } from '$lib/server/db/schema';
-import { desc, eq } from 'drizzle-orm';
-import type { Actions, PageServerLoad } from './$types';
+import { postsTable } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
+import type { PageServerLoad } from './$types';
 import type { Session } from 'lucia';
-import { fail } from '@sveltejs/kit';
 
 export const load = (async ({ locals }) => {
 	const posts = await db.query.postsTable.findMany({
@@ -15,7 +14,8 @@ export const load = (async ({ locals }) => {
 					admin: true
 				}
 			}
-		}
+		},
+		orderBy: (postsTable, { desc }) => [desc(postsTable.createdAt)]
 	});
 
 	const session: Session = await locals.auth.validate();
@@ -24,12 +24,3 @@ export const load = (async ({ locals }) => {
 		session
 	};
 }) satisfies PageServerLoad;
-
-export const actions = {
-	default: async ({ request }) => {
-		const formData = await request.formData();
-		const id = formData.get('postId')!.toString();
-		const convId = Number(id);
-		await db.delete(postsTable).where(eq(postsTable.id, convId));
-	}
-} satisfies Actions;
