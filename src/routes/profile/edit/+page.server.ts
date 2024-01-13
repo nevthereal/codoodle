6a6a4997ca6e3d19c76b68244e426ebb7012cc3d
Neville Brem <bremneville@gmail.com>
2024-01-13 15:13:23 +0100

    feat: implemented username change

--- src/routes/profile/edit/+page.server.ts
+++ src/routes/profile/edit/+page.server.ts
@@ -1,8 +1,50 @@
-import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import { fail, redirect } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
+import { z } from 'zod';
+import { setError, superValidate } from 'sveltekit-superforms/server';
+import { db } from '$lib/server/db/db';
+import { eq } from 'drizzle-orm';
+import { usersTable } from '$lib/server/db/schema';
+import type { Session } from 'lucia';
+
+const username = z.object({
+	username: z
+		.string({ required_error: 'Username is Required' })
+		.trim()
+		.toLowerCase()
+		.min(3, { message: 'Your username has to be 3 characters long' })
+		.max(32, { message: 'Your username is too long' })
+		.regex(new RegExp('^[a-zA-Z0-9_]*$'), {
+			message: 'Username can only contain letters, numbers or an underscore'
+		})
+});
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth.validate(); // Validates the session
 	if (!session) throw redirect(302, '/signin');
-	return { session };
+	const form = await superValidate(username);
+	return { session, form };
 };
+
+export const actions = {
+	default: async ({ request, locals }) => {
+		const session: Session = locals.auth.validate();
+		if (!session) throw redirect(302, '/signin');
+
+		const form = await superValidate(request, username);
+		if (!form.valid) return fail(400, { form });
+
+		if (
+			await db.query.usersTable.findFirst({
+				where: eq(usersTable.username, form.data.username)
+			})
+		) {
+			return setError(form, 'username', 'Username is already exists');
+		}
+
+		// await auth.updateUserAttributes(session.user.userId, {
+		// 	username: form.data.username
+		// });
+		// session.invalidate();
+	}
+} satisfies Actions;
--- src/routes/profile/edit/+page.svelte
+++ src/routes/profile/edit/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+	import type { PageData } from './$types';
+	import { superForm } from 'sveltekit-superforms/client';
 
 	const modalStore = getModalStore();
 
@@ -20,9 +22,24 @@
 			goto('/');
 		});
 	};
+
+	export let data: PageData;
+	const { form, enhance, errors } = superForm(data.form, {
+		resetForm: true
+	});
 </script>
 
 <h1 class="h1 mb-8">Edit your profile</h1>
+<form use:enhance method="POST" class="my-16 flex flex-col gap-2 justify-start">
+	<div>
+		<label for="username">Update username:</label>
+		<input type="text" name="username" />
+		{#if $errors.username}
+			<span class="text-error-500">{$errors.username}</span>
+		{/if}
+	</div>
+	<button class="btn variant-ghost-primary mr-auto">Update</button>
+</form>
 <button class="btn variant-ghost-error" on:click={() => modalStore.trigger(dM)}>
 	Delete your account
 </button>
