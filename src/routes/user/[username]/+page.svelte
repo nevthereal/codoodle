ca69e1b7cc70aae254bb4e2a5c878910d601e410
Neville Brem <contact@nevillebrem.com>
2024-01-13 19:44:02 +0100

    style: styled profile page a bit

--- src/routes/user/[username]/+page.svelte
+++ src/routes/user/[username]/+page.svelte
@@ -7,14 +7,16 @@
 	const user = data.user;
 </script>
 
-<h1 class="h1">{user.username}</h1>
-<h4 class="h4">
-	Number of posts: <span
-		class={`${
-			user.posts.length < 5 ? 'text-grey-500' : user.posts.length < 10 ? 'text-amber-500' : ''
-		} `}>{user.posts.length}</span
-	>
-</h4>
+<div class="flex flex-col gap-2 my-4">
+	<h1 class="h1">{user.username}</h1>
+	<h4 class="h4">
+		Number of posts: <span
+			class={`${
+				user.posts.length < 5 ? 'text-grey-500' : user.posts.length < 10 ? 'text-amber-500' : ''
+			} `}>{user.posts.length}</span
+		>
+	</h4>
+</div>
 <div class="flex flex-col gap-4">
 	{#each user.posts as post}
 		<Post {post} currentUserId={user.id} />
