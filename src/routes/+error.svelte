51dc5cd55b4980ec922435bef6e2f67d89c22b21
Neville Brem <bremneville@gmail.com>
2024-01-20 19:53:48 +0100

    feat: added proper error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="flex flex-col text-center gap-4">
+	<h1 class="h1">Something went wrong ...</h1>
+	<p class="">{$page.status} - {$page.error?.message}</p>
+	<a href="/" class="btn variant-ghost-primary mx-auto">Go to Home</a>
+</div>
