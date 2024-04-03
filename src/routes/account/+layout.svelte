9abed1cee95126f3526fc64f60b5ad7c1e36ec71
Neville Brem <bremneville@gmail.com>
2024-04-03 19:18:00 +0200

    some other fixes to accout page

--- src/routes/account/+layout.svelte
+++ src/routes/account/+layout.svelte
@@ -9,34 +9,31 @@
 
 <Modal />
 
-<div class="flex md:flex-row flex-col-reverse">
+<div class="flex md:flex-row flex-col">
 	<nav
-		class="rounded-container-token md:bg-surface-500 p-6 font-semibold border-t-surface-500 border-t"
+		class="md:rounded-container-token md:bg-surface-500 p-6 font-semibold border-b-surface-500 border-b-2"
 	>
 		<ul class="flex flex-col gap-2">
-			<li class="flex gap-2 items-center">
-				<i class="fa-solid fa-user md:hidden"></i><a
-					class="md:btn text-xl md:text-base"
-					href="/account">Overview</a
+			<li>
+				<a class="md:btn text-xl md:text-base flex gap-2 items-center" href="/account"
+					><i class="fa-solid fa-user md:hidden"></i>Overview</a
 				>
 			</li>
-			<li class="flex gap-2 items-center">
-				<i class="fa-solid fa-file md:hidden"></i><a
-					class="md:btn text-xl md:text-base"
-					href="/account/posts">Posts</a
+			<li>
+				<a class="md:btn text-xl md:text-base flex gap-2 items-center" href="/account/posts">
+					<i class="fa-solid fa-file md:hidden"></i>Posts</a
 				>
 			</li>
-			<li class="flex gap-2 items-center">
-				<i class="fa-solid fa-pencil md:hidden"></i><a
-					class="md:btn text-xl md:text-base"
-					href="/account/edit">Edit</a
+			<li>
+				<a class="md:btn text-xl md:text-base flex gap-2 items-center" href="/account/edit"
+					><i class="fa-solid fa-pencil md:hidden"></i>Edit</a
 				>
 			</li>
-			<li class="flex gap-2 items-center">
-				<i class="fa-solid fa-door-open text-error-500 md:hidden"></i><a
+			<li>
+				<a
 					href="/api/signout"
-					class="md:btn text-xl md:text-base md:variant-ghost-error text-error-500 md:text-white"
-					>Sign out</a
+					class="md:btn text-xl md:text-base md:variant-ghost-error text-error-500 md:text-white flex gap-2 items-center"
+					><i class="fa-solid fa-door-open text-error-500 md:hidden"></i>Sign out</a
 				>
 			</li>
 		</ul>
