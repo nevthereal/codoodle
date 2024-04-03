9ee075be97870f776cf31d9d884624463aee00ed
Neville Brem <bremneville@gmail.com>
2024-04-03 19:14:37 +0200

    updated nav on account page

--- src/routes/account/+layout.svelte
+++ src/routes/account/+layout.svelte
@@ -1,5 +1,5 @@
-<script>
-	import { AppShell, Modal, initializeStores } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import { Modal, initializeStores } from '@skeletonlabs/skeleton';
 	initializeStores();
 </script>
 
@@ -8,26 +8,40 @@
 </svelte:head>
 
 <Modal />
-<AppShell>
-	<svelte:fragment slot="sidebarLeft">
-		<nav class="rounded-container-token bg-surface-500 p-6 flex flex-col gap-2 font-semibold">
-			<ul class="flex flex-col gap-2">
-				<li>
-					<a class="btn" href="/account">Overview</a>
-				</li>
-				<li>
-					<a class="btn" href="/account/posts">Posts</a>
-				</li>
-				<li>
-					<a class="btn" href="/account/edit">Edit</a>
-				</li>
-				<li>
-					<a href="/api/signout" class="btn variant-ghost-error">Sign out</a>
-				</li>
-			</ul>
-		</nav>
-	</svelte:fragment>
-	<main class="p-8">
+
+<div class="flex md:flex-row flex-col-reverse">
+	<nav
+		class="rounded-container-token md:bg-surface-500 p-6 font-semibold border-t-surface-500 border-t"
+	>
+		<ul class="flex flex-col gap-2">
+			<li class="flex gap-2 items-center">
+				<i class="fa-solid fa-user md:hidden"></i><a
+					class="md:btn text-xl md:text-base"
+					href="/account">Overview</a
+				>
+			</li>
+			<li class="flex gap-2 items-center">
+				<i class="fa-solid fa-file md:hidden"></i><a
+					class="md:btn text-xl md:text-base"
+					href="/account/posts">Posts</a
+				>
+			</li>
+			<li class="flex gap-2 items-center">
+				<i class="fa-solid fa-pencil md:hidden"></i><a
+					class="md:btn text-xl md:text-base"
+					href="/account/edit">Edit</a
+				>
+			</li>
+			<li class="flex gap-2 items-center">
+				<i class="fa-solid fa-door-open text-error-500 md:hidden"></i><a
+					href="/api/signout"
+					class="md:btn text-xl md:text-base md:variant-ghost-error text-error-500 md:text-white"
+					>Sign out</a
+				>
+			</li>
+		</ul>
+	</nav>
+	<main class="p-6">
 		<slot />
 	</main>
-</AppShell>
+</div>
